c782c7139740766a35d025ccb36a0e9212f7823d
takakabe <takakabe5384@gmail.com>
2019-11-07 07:08:52 +0900

    add error handring

--- uncomp_s3.py
+++ uncomp_s3.py
@@ -1,47 +1,59 @@
 import boto3
+from botocore.exceptions import ClientError
 import zipfile
-import gzip
 import tarfile
 from io import BytesIO
 import argparse
 import re
 import copy
+import sys
 
 class uncomp():
     def __init__(self, path):    
-        self.path_list = path[5:].split('/')
-        self.bucket_name = self.path_list[0]
-        self.file_name = self.path_list[-1]
-        self.zip_obj = BytesIO(s3.Object(bucket_name=self.bucket_name, key=self.file_name).get()["Body"].read())
-        self.tmp_zip_obj = copy.copy(self.zip_obj)
-        
-        if zipfile.is_zipfile(self.tmp_zip_obj):
-            self.zip()
-        else:
-            self.tar()
+        try:
+            self.path_list = path[5:].split('/')
+            self.bucket_name = self.path_list[0]
+            self.file_name = self.path_list[-1]
+            self.object = BytesIO(s3.Object(bucket_name=self.bucket_name, key=self.file_name).get()["Body"].read())
+            self.tmp_object = copy.copy(self.object)
+        except ClientError :
+            print('No such key.')
+            sys.exit(1)
+
+        try:
+            if zipfile.is_zipfile(self.tmp_object):
+                self.zip()
+            else :
+                self.tar()
+        except:
+            print(self.file_name)
+            sys.exit(1)
 
     def zip(self):    
-        zip = zipfile.ZipFile(self.zip_obj)
+        zip = zipfile.ZipFile(self.object)
         for filename in zip.namelist():
             file_info = zip.getinfo(filename)
             print(filename)
 
     def tar(self):
-        tar = tarfile.open(fileobj=self.zip_obj)
+        tar = tarfile.open(fileobj=self.object)
         for filename in tar.getnames():
             print(filename)
         tar.close()
 
-if __name__ == '__main__':
-    s3 = boto3.resource('s3')
 
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog=__file__)
     parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
     required.add_argument('path', help='example s3://mybucket/wawawa.zip', type=str)
-    #optional = parser.add_argument_group('optional arguments')
-    #optional.add_argument('--name', default='ALL', help='Name of AWS account (default: ALL)')
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument('--profile',nargs='?',default='default',help='Use a specific profile from your credential file.') 
+
     args = parser.parse_args()
-    
+
+    session = boto3.Session(profile_name=args.profile)
+    s3 = session.resource('s3')
+
     uncomp(args.path)
 
